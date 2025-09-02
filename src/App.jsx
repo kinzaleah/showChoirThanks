cf1265150e9bbcbcabbb55ef647650cd3c9ab26d
Kinza Drewett <kinzadrewett@gmail.com>
2025-09-02 21:45:22 +0100

    added pictures to comments

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,37 @@ import Landing from "./Landing";
 import BackHomeIcon from "./BackHomeIcon";
 import thanksData from "./thanksData";
 
+// List of photo filenames in src/photos (relative to public or src for Vite)
+const PHOTOS = [
+  "2025-08-21 17_22_25 X-T30 II 16mm f4.0 1_125.jpg",
+  "2025-08-23 10_45_00 X-T30 II 34mm f8.0 1_500.jpg",
+  "2025-08-23 11_26_28 X-T30 II 80mm f6.4 1_125.jpg",
+  "2025-08-23 11_28_33 X-T30 II 80mm f4.0 1_125.jpg",
+  "2025-08-23 11_32_15 X-T30 II 80mm f4.0 1_125.jpg",
+  "2025-08-23 11_49_36 X-T30 II 80mm f4.0 1_200.jpg",
+  "2025-08-23 11_51_32 X-T30 II 48mm f5.6 1_140.jpg",
+  "20250819_182439 Jacquie Kelly.jpg",
+  "20250819_182540 Jacquie Kelly.jpg",
+  "20250819_183422 Cassie Brooks.jpg",
+  "63CE24AC-0CE3-4743-9C68-59362B7BF18D Cath Potter.jpeg",
+  "7FB23B8A-AA0B-43DB-AC91-6E7C6E7517F8 Cath Potter.jpeg",
+  "AD52871F-418F-45F9-AC50-AE61CCE41021 Cath Potter.jpeg",
+  "FAC857BD-15DE-4E63-BF9C-C7605DBE0611 Cath Potter.jpeg",
+  "IMG_0499 Fay Winfield.JPEG",
+  "IMG_0739 Fay Winfield.JPEG",
+  "IMG_0869 Fay Winfield.JPEG",
+  "IMG_4332 Show Choir.jpeg",
+  "IMG_7517 Jack Clifton.jpeg",
+  "PXL_20250819_101136433 Claire.jpg",
+  "PXL_20250819_163747842 Claire.jpg",
+  "PXL_20250821_172900148 Kinza.jpg",
+  "PXL_20250822_092522837 Kinza.jpg",
+  "PXL_20250823_095037502 Kinza.jpg",
+  "WhatsApp Image 2025-09-02 at 21.32.40_1273b700.jpg",
+  "WhatsApp Image 2025-09-02 at 21.32.40_e23cde48.jpg",
+  "_DSF8518.jpg",
+];
+
 // Use 3 comments per page to avoid overlap
 const DEFAULT_COMMENTS_PER_PAGE = 3;
 const PLACEHOLDER_PHOTO = "https://picsum.photos/400/400?grayscale&random=1";
@@ -58,13 +89,18 @@ function App() {
     return <Landing onEnter={() => setShowLanding(false)} />;
   }
 
+
+  // Pick a photo for this page, cycling if more pages than photos
+  const photoIdx = (page - 1) % PHOTOS.length;
+  const photoSrc = `/src/photos/${PHOTOS[photoIdx]}`;
+
   return (
     <>
       <BackHomeIcon onClick={() => setShowLanding(true)} />
       <div className="container">
         <div className="grid">
           <div className="photo">
-            <img src={PLACEHOLDER_PHOTO} alt="Placeholder" />
+            <img src={photoSrc} alt={`Show Choir ${photoIdx + 1}`} />
           </div>
           <div className="comments">
             {comments.map((entry, idx) => (
